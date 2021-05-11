c1de70e4300e9781d665488da913002df0b45d44
dominic158 <84009777+dominic158@users.noreply.github.com>
2021-05-11 16:23:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'c218ae7332d64c15847c8faece013266',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '博客名称',
+  domain: 'https://www.notion.so/Web-Clipper-c218ae7332d64c15847c8faece013266',
+  author: '博客作者',
 
   // open graph metadata (optional)
   description: 'Example site description',
